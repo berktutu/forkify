da7d79e9f6313134fd3612b380d8b9640376b697
berktutu <brktutu@gmail.com>
2024-08-17 19:45:00 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fae6c4c9-57fb-44ef-9fac-5ddd2a27d896';
+export const MODAL_CLOSE_SEC = 2.5;
